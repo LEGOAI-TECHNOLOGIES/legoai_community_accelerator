f73b29a8181af93f93f4941d45f33cd1199d003b
narotsitk <narotsitk2001@gmail.com>
2024-01-20 14:50:35 +0545

    Added core files necessary for program functionality. - added logger to create a log object. - added path configuration to get required path for the program. - add ML model config parameters necessary to run the model properly.

---
+++ core/model_configuration.py
@@ -0,0 +1,37 @@
+import yaml
+from core.path_configuration import PATH_CONFIG
+import os
+import sys
+
+def load_default_model_configuration(file_path:str = 'legoai',file_name:str = "config.yaml"):
+    model_yaml_path = os.path.join(file_path,file_name)
+    config = {}
+    try:       
+        with open(model_yaml_path,"r") as default_config_file:
+            config = yaml.safe_load(default_config_file)
+        return config
+    
+    except FileNotFoundError:
+        print(f"\n[!] default configuration file not found at {model_yaml_path}")
+        sys.exit(-1)
+
+
+MODEL_CONFIG = load_default_model_configuration()
+
+def load_model_configuration(file_path:str,file_name:str):
+
+    model_yaml_path = os.path.join(file_path,file_name)
+    try:
+        if not file_name.endswith(".yaml"):
+            raise Exception("[!] configuration file must be yaml")
+        else:
+            with open(model_yaml_path,"r") as custom_config_file:
+                MODEL_CONFIG = yaml.safe_load(custom_config_file)
+                print("[*] loaded custom model configuration")
+
+    except Exception as ex:
+        print(ex.__str__() + "... loading default configuration")
+
+        MODEL_CONFIG = load_default_model_configuration()
+
+
---
+++ core/path_configuration.py
@@ -0,0 +1,34 @@
+from dotenv import dotenv_values
+import os
+
+def prepare_path_configuration():
+    file_abs_path = os.path.abspath(os.path.dirname(__file__))
+    default_path_config = {
+        "CONTAINER_PATH" : file_abs_path.removesuffix("\core")+ "/legoai/Lego_AI",
+        "GT_PATH" : "data/input/ground_truth",
+        "RAW_DATA_PATH":"data/input/raw_data",
+        "DEV_REF_DATA_PATH":"data/input/reference_data",
+        "INT_DATA_PATH":"data/intermediate",
+        "ANALYT_DATA_PATH": "data/analytical_data",
+        "MODEL_DEP_PATH":"model/dependant",
+        "MODEL_METRICS_PATH":"model/model_metrics",
+        "MODEL_OBJECTS_PATH":"model/model_objects",
+        "MODEL_RESULTS_PATH":"model/model_results",
+        "INF_DATA_PATH":"inference/input",
+        "INF_OUT_PATH":"inference/test_output",
+        "INF_REF_DATA_PATH":"inference/reference_files",
+        "LOG_PATH":"logs",
+        "CODE_PATH": "../legoai/datatype_identification",
+        "LOG_LEVEL":"info"
+    }
+
+
+    loaded_path_config = dotenv_values(".env")
+    
+    return {
+        **default_path_config,
+        # **loaded_path_config
+    }
+
+
+PATH_CONFIG = prepare_path_configuration()
