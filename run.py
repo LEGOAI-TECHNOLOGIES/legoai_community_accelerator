7fabf7755f91e584dcba1fbaf1cade3462489ef4
narotsitk <narotsitk2001@gmail.com>
2024-01-21 23:38:50 +0545

    trying to run default model

--- run.py
+++ run.py
@@ -2,8 +2,11 @@ from legoai.pipeline import DataTypeIdentificationPipeline
 from legoai.core.model_configuration import MODEL_CONFIG
 
 if __name__ == "__main__":
-    dataset_name = "ecommerce_data"
-    di_pipeline = DataTypeIdentificationPipeline(dataset_name=dataset_name,open_api_key= MODEL_CONFIG["L3PARAMS"]["OPENAI_API_KEY"])
-    prediction = di_pipeline.model_prediction(save_result=False)
-    print(prediction[['column_name_clean','predicted_datatype_l1','predicted_datatype_l3']].head())
-
+    
+       
+        dataset_name = "ecommerce_data"
+    
+        di_pipeline = DataTypeIdentificationPipeline(dataset_name=dataset_name,open_api_key= MODEL_CONFIG["L3PARAMS"]["OPENAI_API_KEY"])
+        # # prediction = DataTypeIdentificationPipeline.load_default_model(open_api_key="")
+        prediction = di_pipeline.run_pipeline(save_result=False)
+        print(prediction[['column_name_clean','predicted_datatype_l1','predicted_datatype_l3']].head())
