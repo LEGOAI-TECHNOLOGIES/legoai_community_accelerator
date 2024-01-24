e9569a0906e8cde69a86b93241f7f4c643c5b78a
narotsitk <narotsitk2001@gmail.com>
2024-01-25 01:05:05 +0545

    some few modifications

--- run.py
+++ run.py
@@ -1,12 +1,11 @@
 from legoai.pipeline import DataTypeIdentificationPipeline
-from legoai.core.model_configuration import MODEL_CONFIG
+from legoai.modules.datatype_identification import MODEL_CONFIG
+
 
 if __name__ == "__main__":
-    
-       
-        dataset_name = "ecommerce_data"
-    
-        di_pipeline = DataTypeIdentificationPipeline(dataset_name=dataset_name,open_api_key= MODEL_CONFIG["L3PARAMS"]["OPENAI_API_KEY"])
-        # # prediction = DataTypeIdentificationPipeline.load_default_model(open_api_key="")
-        prediction = di_pipeline.run_pipeline(save_result=False)
-        print(prediction[['column_name_clean','predicted_datatype_l1','predicted_datatype_l3']].head())
+    di_pipeline = DataTypeIdentificationPipeline(
+        dataset_name="ecommerce_data",
+        openai_api_key= MODEL_CONFIG["L3PARAMS"]["OPENAI_API_KEY"] 
+        ) 
+   
+    di_pipeline.run_pretrained_pipeline(save_to="output.csv")
