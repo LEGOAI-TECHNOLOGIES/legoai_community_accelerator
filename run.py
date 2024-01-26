ffc177f783c237e01374b3f7fcc075aff2655b6d
narotsitkarki <naro.karki@legoai.com>
2024-01-26 10:09:54 +0545

    .

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 from legoai.pipeline import DataTypeIdentificationPipeline
 from legoai.modules.datatype_identification import MODEL_CONFIG
 
-
 if __name__ == "__main__":
     di_pipeline = DataTypeIdentificationPipeline.pretrained_pipeline(open_api_key=MODEL_CONFIG["L3PARAMS"]["OPENAI_API_KEY"])
     result = di_pipeline(dataset_name="ecommerce_data")
