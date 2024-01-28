121658d1be893548c1a19521f9ccb9e7c0399a35
narotsitkarki <naro.karki@legoai.com>
2024-01-28 22:29:52 +0545

    test ran on new modification -- success

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
 from legoai import DataTypeIdentificationPipeline
+from legoai.utils import download_default_dataset
 from legoai.core.configuration import MODEL_CONFIG
-
+# this is an example of how to run a pretrained pipeline
 if __name__ == "__main__":
-    di_pipeline = DataTypeIdentificationPipeline.pretrained_pipeline(open_api_key=MODEL_CONFIG["L3PARAMS"]["OPENAI_API_KEY"])
-    di_pipeline.
-    result = di_pipeline(dataset_name="ecommerce_data",save_to="di_output.csv")
+    dataset_name = download_default_dataset()
+    # print(dataset_name)
+    di_pipeline = DataTypeIdentificationPipeline.pretrained_pipeline(openai_api_key=MODEL_CONFIG["L3PARAMS"]["OPENAI_API_KEY"])
+    result = di_pipeline(dataset_name=dataset_name,save_to="di_output.csv")
     print(result[['column_name_clean','predicted_datatype_l1','predicted_datatype_l3']].head())
