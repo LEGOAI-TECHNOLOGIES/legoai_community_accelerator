8c65319a109cadc71c7f0c4b03fd9d6db95c3044
narotsitkarki <naro.karki@legoai.com>
2024-01-28 13:46:37 +0545

    moved pipeline code to di module and refactored run.py to accomodated that change

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
-from legoai.pipeline import DataTypeIdentificationPipeline
-from legoai.modules.datatype_identification import MODEL_CONFIG
+from legoai import DataTypeIdentificationPipeline
+from legoai.core.configuration import MODEL_CONFIG
 
 if __name__ == "__main__":
     di_pipeline = DataTypeIdentificationPipeline.pretrained_pipeline(open_api_key=MODEL_CONFIG["L3PARAMS"]["OPENAI_API_KEY"])
-    result = di_pipeline(dataset_name="ecommerce_data")
-    print(result[['column_name_clean','predicted_datatype_l1','predicted_datatype_l3']])
+    di_pipeline.
+    result = di_pipeline(dataset_name="ecommerce_data",save_to="di_output.csv")
+    print(result[['column_name_clean','predicted_datatype_l1','predicted_datatype_l3']].head())
