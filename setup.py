a17fb4ffbb51b7c42985ab065842b1758f4f3d26
narotsitkarki <naro.karki@legoai.com>
2024-04-10 10:37:04 +0545

    setup for v0.3 of the package

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ with open("README.md", "r") as f:
 with open("requirements.txt", "r") as f:
     requirements = f.read().strip().split("\n")
 
-VERSION = '0.0.1'
+VERSION = '0.3'
 setup(
-    name="legoai_di",
+    name="legoai",
     version=VERSION,
-    description="An open-source package for identifying data types",
+    description="An open-source package from LEGOAI for identifying data types",
     packages=find_packages(),
     package_data= {
         '': ['config.yaml','model/dependant/datatype_l1_identification/*','model/model_objects/datatype_l1_identification/*.pkl'],
@@ -22,14 +22,14 @@ setup(
     author_email="contactus@legoai.com",
     license="MIT",
     classifiers=[
-        "License :: MIT License",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent",
         "Environment :: Console",
     ],
     install_requires=requirements,
     extras_require={
-        "dev": ["twine >= 4.0.2"]
+        "dev": ["twine>=4.0.2"],
     },
     python_requires=">=3.10",
 )
